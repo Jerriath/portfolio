6b86eb98339ecede831acc4fc57d103b0ab048bc
Jerry Zhang <jerryzhang101@yahoo.com>
2022-08-03 16:10:17 -0400

    Added about component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 // Importing node modules
 import React from 'react'
+import { useEffect } from 'react';
 
 // Importing local project files
 import Homepage from './components/home/Homepage';
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 const App = () => {
-  return (
-    <main className='app'>
-        <Header />
-        <Homepage />
-    </main>
-  )
+
+    useEffect(() => {
+        const handleScroll = event => {
+          console.log('window.scrollY', window.scrollY);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+    return (
+        <main className='app'>
+            <Header />
+            <Homepage />
+            <About />
+        </main>
+    )
 }
 
 export default App
